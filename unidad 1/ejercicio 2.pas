1d5c6b3fd58d605fd4bf424734b379d80c9962cf
Alfredo Rodolfo Sara <89542983+rodolfoSara@users.noreply.github.com>
2023-07-31 20:08:18 -0300

    Add files via upload

---
+++ unidad 1/ejercicio 2.pas
@@ -0,0 +1,14 @@
+program ejercicio1;
+var
+  num1: integer;
+  num2: integer;
+  resultado: integer;
+  
+begin
+  
+  readln(num1);
+  readln(num2);
+  resultado:= (num1 + num2);
+  writeln(resultado);
+  
+end.
